77e211787ddb982fb2731a97f7a1ab30bd422925
Unknown <kmiloandres9605@gmail.com>
2018-03-31 20:38:54 -0500

    View

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CRM GRADUADOS 
                 </div>
 
                 <div class="links">
