429c67f9273b82df6b83e8e8ef6391ee26ffacdb
Unknown <kmiloandres9605@gmail.com>
2018-03-31 20:53:06 -0500

    Gabriel la cago

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Bienvenidos
+                    TODO ES CULPA DE GABRIEL......
                 </div>
 
                 <div class="links">
