88fedbb46b15f147f8faf757b2e2bc598ae3636c
Unknown <kmiloandres9605@gmail.com>
2018-03-31 21:11:42 -0500

    yupiiiiiiii

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    TODO ES CULPA DE GABRIEL......
+                    encontramos la solucion me voy a salar
                 </div>
 
                 <div class="links">
